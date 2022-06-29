1f9814b99186c472b55ea30a45ab96eba058d35d
Morgan Wilde <morgan@wilde.mx>
2022-06-29 11:59:20 +1000

    Seperated navigation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
+
 import Home from './pages/Home'
 import CategoryRecommended from './pages/CategoryRecommended'
 import CategoryLibrary from './pages/CategoryLibrary'
@@ -8,27 +10,44 @@ import Episode from './pages/Episode'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
 import Season from './pages/Season'
-import Navigation from 'components/Navigation'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
 
 function App() {
+  const [shrinkSidebar, setShrinkSidebar] = useState(false)
+
   return (
     <>
       <Router>
-        <Navigation />
-        <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/:category' element={<CategoryRecommended />}></Route>
-          <Route
-            path='/:category/library'
-            element={<CategoryLibrary />}
-          ></Route>
-          <Route path='/:movie' element={<Movie />}></Route>
-          <Route path='/:tvshow' element={<TvShow />}></Route>
-          <Route path='/:tvshow/:season' element={<Season />}></Route>
-          <Route path='/:tvshow/:season/:episode' element={<Episode />}></Route>
-          <Route path='/about' element={<About />}></Route>
-          <Route path='/*' element={<NotFound />}></Route>
-        </Routes>
+        <Navbar
+          setShrinkSidebar={setShrinkSidebar}
+          shrinkSidebar={shrinkSidebar}
+        />
+        <div className='flex'>
+          <Sidebar shrinkSidebar={shrinkSidebar} />
+          <div className='relative content'>
+            <Routes>
+              <Route path='/' element={<Home />}></Route>
+              <Route
+                path='/:category'
+                element={<CategoryRecommended />}
+              ></Route>
+              <Route
+                path='/:category/library'
+                element={<CategoryLibrary />}
+              ></Route>
+              <Route path='/:movie' element={<Movie />}></Route>
+              <Route path='/:tvshow' element={<TvShow />}></Route>
+              <Route path='/:tvshow/:season' element={<Season />}></Route>
+              <Route
+                path='/:tvshow/:season/:episode'
+                element={<Episode />}
+              ></Route>
+              <Route path='/about' element={<About />}></Route>
+              <Route path='/*' element={<NotFound />}></Route>
+            </Routes>
+          </div>
+        </div>
       </Router>
     </>
   )
